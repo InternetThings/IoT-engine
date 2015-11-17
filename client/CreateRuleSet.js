b982c68b08f38c8dd2fc0507fa0bcb9cff18d07b
JLE85 <eaajeesk@students.eaaa.dk>
2015-11-17 10:23:27 +0100

    RuleSetTest done, corrections made to EvaluateCondition function.

--- client/CreateRuleSet.js
+++ client/CreateRuleSet.js
@@ -1,7 +1,7 @@
 Template.CreateRuleSet.events({
   'click #AddConditionbtn': function(event) {
     event.preventDefault();
-    var sensor = sensors.find();
+    
   },
 
   'click #SaveRuleSetbtn': function(event) {
